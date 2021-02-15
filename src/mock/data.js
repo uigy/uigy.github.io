227fdb24784ad47b96a805ab1eafa5e6ad380ddf
uigy <uigy@pm.me>
2021-02-15 17:09:49 +0100

    updated

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: 'Tomek Jabko | Frontend developer', // e.g: 'Name | Developer'
+  title: 'Tomek Jabko | Frontend Developer', // e.g: 'Name | Developer'
   lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
